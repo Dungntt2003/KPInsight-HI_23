1f0cf0e514413d71494930e962cba85a2a116693
cuhp293 <phuchong292003@gmail.com>
2024-05-16 03:27:35 +0700

    update target detail page: add button interact

--- kpinsight/src/view/pages/target/target_detail/index.jsx
+++ kpinsight/src/view/pages/target/target_detail/index.jsx
@@ -1,54 +1,30 @@
 import "./index.css";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Select, Tag, message, Popconfirm, Button, Checkbox } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Select,
+  Tag,
+  message,
+  Popconfirm,
+  Button,
+  Checkbox,
+  Modal,
+} from "antd";
 import {
   EllipsisOutlined,
   CloseCircleOutlined,
   StarOutlined,
   StarFilled,
+  DeleteOutlined,
 } from "@ant-design/icons";
 import NavbarGoal from "../../../../components/navbar/navbar-goal";
 
 function Target_list() {
-  //Choose label - select
-  const handleLabel = (value) => {
-    console.log(value);
-  };
-
-  //Star
-  const handleStarClick = (id) => {
-    setStudies(
-      studies.map((study) =>
-        study.id === id ? { ...study, star: !study.star } : study
-      )
-    );
-    setSocials(
-      socials.map((social) =>
-        social.id === id ? { ...social, star: !social.star } : social
-      )
-    );
-    setPersonas(
-      personas.map((persona) =>
-        persona.id === id ? { ...persona, star: !persona.star } : persona
-      )
-    );
-  };
-
-  //Pop confirm
-  const confirm = (e) => {
-    console.log(e);
-    message.success("Đã xóa mục tiêu!");
-  };
-  const cancel = (e) => {
-    console.log(e);
-    message.error("Hủy xóa mục tiêu!");
-  };
-
   //Data
   const [studies, setStudies] = useState([
     {
       id: 0,
+      checked: false,
       content: "Đạt 3.0/4.0 kì 2023.2",
       // tag: (
       //     <Tag id=""color="#074979"></Tag>
@@ -58,6 +34,7 @@ function Target_list() {
     },
     {
       id: 1,
+      checked: false,
       content: "Đạt chứng chỉ TOEIC 500+",
       tag: (
         <Tag id="tag-nn" color="magenta">
@@ -69,6 +46,7 @@ function Target_list() {
     },
     {
       id: 2,
+      checked: false,
       content: "Đạt giải cuộc thi She Codes 2023",
       tag: (
         <Tag id="tag-it" color="red">
@@ -80,6 +58,7 @@ function Target_list() {
     },
     {
       id: 9,
+      checked: true,
       content: "Đạt KPA 3.0/4.0 kì 2022.2",
       // tag: (
       //   <Tag id="tag-it" color="red">
@@ -91,6 +70,7 @@ function Target_list() {
     },
     {
       id: 10,
+      checked: true,
       content: "Đạt chứng chỉ JLPT N3",
       tag: (
         <Tag id="tag-nn" color="magenta">
@@ -102,6 +82,7 @@ function Target_list() {
     },
     {
       id: 11,
+      checked: true,
       content: "Hoàn thành khóa học ReactJS",
       tag: (
         <Tag id="tag-it" color="red">
@@ -179,6 +160,91 @@ function Target_list() {
     },
   ]);
 
+  //Choose label - select
+  const handleLabel = (value) => {
+    console.log(value);
+  };
+
+  //Star
+  const handleStarClick = (id) => {
+    setStudies(
+      studies.map((study) =>
+        study.id === id ? { ...study, star: !study.star } : study
+      )
+    );
+    setSocials(
+      socials.map((social) =>
+        social.id === id ? { ...social, star: !social.star } : social
+      )
+    );
+    setPersonas(
+      personas.map((persona) =>
+        persona.id === id ? { ...persona, star: !persona.star } : persona
+      )
+    );
+  };
+
+  //Pop confirm
+  const confirmPop = (e) => {
+    console.log(e);
+    message.success("Đã xóa mục tiêu!");
+  };
+  const cancelPop = (e) => {
+    console.log(e);
+    message.error("Hủy xóa mục tiêu!");
+  };
+
+  //Check box
+  const [checkedItems, setCheckedItems] = useState(
+    studies.filter((study) => study.checked).map((study) => study.id)
+  );
+  const onChangeCheckBox = (itemId) => {
+    setCheckedItems((prev) =>
+      prev.includes(itemId)
+        ? prev.filter((id) => id !== itemId)
+        : [...prev, itemId]
+    );
+  };
+
+  //Delete confirm - modal
+  const navigate = useNavigate();
+  const LocalizedModal = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const showModal = () => {
+      setOpenModal(true);
+    };
+    const handleModalCancel = () => {
+      setOpenModal(false);
+      message.error("Hủy xóa mục tiêu!");
+    };
+    const handleModalOk = () => {
+      setOpenModal(false);
+      navigate("/target_list");
+      message.success("Đã xóa mục tiêu!");
+    };
+    return (
+      <>
+        <span
+          onClick={showModal}
+          className="target-list-icon"
+          id="goal-list-title-icon"
+        >
+          <CloseCircleOutlined />
+        </span>
+        <Modal
+          title="Xóa mục tiêu"
+          open={openModal}
+          onOk={handleModalOk}
+          onCancel={handleModalCancel}
+          okText="Xác nhận"
+          cancelText="Hủy"
+        >
+          <p>Xác nhận xóa mục tiêu</p>
+        </Modal>
+      </>
+    );
+  };
+
   return (
     <div className="target-list-container">
       <NavbarGoal />
@@ -247,33 +313,29 @@ function Target_list() {
         <div className="target-list-wrap">
           <h3>
             <span id="goal-list-title">Học tập</span>
-            {/* <Link
-              to="/target_detail"
-              className="target-list-icon"
-              id="goal-list-title-icon"
-            >
-              <EditOutlined />
-            </Link> */}
-            <Popconfirm
-              placement="bottomLeft"
-              title="Xóa mục tiêu"
-              description="Xác nhận xóa mục tiêu"
-              onConfirm={confirm}
-              onCancel={cancel}
-              okText="Xác nhận"
-              cancelText="Hủy"
-            >
-              <span className="target-list-icon" id="goal-list-title-icon">
-                <CloseCircleOutlined />
-              </span>
-            </Popconfirm>
+            <LocalizedModal className="target-list-icon" />
           </h3>
 
           <div className="target-list-item">
             <ul id="target-goalList">
               {studies.map((study, index) => (
                 <li id="target-goalList-item" key={index}>
-                  <span className="target-list-content">{study.content}</span>
+                  <Checkbox
+                    id="target-goalList-item-checkbox"
+                    onChangeCheckBox={() => onChangeCheckBox(study.id)}
+                    checked={checkedItems.includes(study.id)}
+                  >
+                    <span
+                      className="target-list-content"
+                      style={{
+                        textDecoration: checkedItems.includes(study.id)
+                          ? "line-through"
+                          : "none",
+                      }}
+                    >
+                      {study.content}
+                    </span>
+                  </Checkbox>
                   <span className="target-list-content-detail">
                     <span className="target-list-tag">{study.tag}</span>
                     <span className="target-list-date">
@@ -289,6 +351,19 @@ function Target_list() {
                         <StarOutlined />
                       )}
                     </span>
+                    <Popconfirm
+                      placement="bottomLeft"
+                      title="Xóa mục tiêu"
+                      description="Xác nhận xóa mục tiêu"
+                      onConfirm={confirmPop}
+                      onCancel={cancelPop}
+                      okText="Xác nhận"
+                      cancelText="Hủy"
+                    >
+                      <span className="target-list-icon">
+                        <DeleteOutlined style={{ color: "black" }} />
+                      </span>
+                    </Popconfirm>
                   </span>
                 </li>
               ))}
@@ -296,7 +371,9 @@ function Target_list() {
             <Link to="/target_detail" className="target-list-icon">
               <EllipsisOutlined />
             </Link>
-            <Button id="target-detail-btn">Lưu và Thoát</Button>
+            <Link to="/target_list" id="target-list-link">
+              <Button id="target-detail-btn">Lưu và Thoát</Button>
+            </Link>
           </div>
         </div>
 
@@ -314,8 +391,8 @@ function Target_list() {
               placement="bottomLeft"
               title="Xóa mục tiêu"
               description="Xác nhận xóa mục tiêu"
-              onConfirm={confirm}
-              onCancel={cancel}
+              onConfirm={confirmPop}
+              onCancel={cancelPop}
               okText="Xác nhận"
               cancelText="Hủy"
             >
@@ -369,8 +446,8 @@ function Target_list() {
               placement="bottomLeft"
               title="Xóa mục tiêu"
               description="Xác nhận xóa mục tiêu"
-              onConfirm={confirm}
-              onCancel={cancel}
+              onConfirm={confirmPop}
+              onCancel={cancelPop}
               okText="Xác nhận"
               cancelText="Hủy"
             >
