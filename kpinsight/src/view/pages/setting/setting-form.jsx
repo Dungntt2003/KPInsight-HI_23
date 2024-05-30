8bfec3c535c9f15488f2d1dc657457819f3bf215
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-05-30 19:13:57 +0700

    Add setting bar

--- kpinsight/src/view/pages/setting/setting-form.jsx
+++ kpinsight/src/view/pages/setting/setting-form.jsx
@@ -6,7 +6,14 @@ function SettingForm(props) {
       <div className="setting-form-mobile">
         <NavbarOption />
       </div>
-      <div>{props.component}</div>
+      <div>
+        <div className="bar-mobile">
+          <div className="bar-account">Tài khoản</div>
+          <div className="bar-setting">Cài đặt chung</div>
+          <div className="bar-help">Trợ giúp</div>
+        </div>
+        <div>{props.component}</div>
+      </div>
     </div>
   );
 }
